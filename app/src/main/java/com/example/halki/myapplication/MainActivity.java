a56d35fb64cc73e91406bd7fb2f4e29f3020e569
Halki-Y <halki@ii.net>
2017-12-08 15:02:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/halki/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.halki.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.coinbase.api.Coinbase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
